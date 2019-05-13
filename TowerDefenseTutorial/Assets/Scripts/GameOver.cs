f1369193fb81742460cbe6db5b0121f4ab59b485
238g <f0rg4tip@gmail.com>
2019-05-13 18:07:53 +0900

    add TowerDefenseTutorial

---
+++ TowerDefenseTutorial/Assets/Scripts/GameOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+	public string menuSceneName = "MainMenu";
+
+	public SceneFader sceneFader;
+
+	public void Retry () {
+		// TODO
+		sceneFader.FadeTo (SceneManager.GetActiveScene().name);
+		// SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void Menu () {
+		// TODO
+		sceneFader.FadeTo (menuSceneName);
+	}
+}
